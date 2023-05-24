6e824931cadd43bf88ce5776f09f20bf72b6dad6
DevExpressExampleBot <fakeEmail>
2023-05-24 15:13:04 +0400

    Source auto update [skip ci]

---
+++ VB/E4787/Models/Entry.vb
@@ -0,0 +1,11 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+
+Namespace E4787.Models
+	Public Class Entry
+		Public Property ID() As Integer
+		Public Property Text() As String
+	End Class
+End Namespace
